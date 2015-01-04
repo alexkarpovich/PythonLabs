44de12225104ace2204e22ee1000ceb22bf6b54f
Yury Chetyrbok <yury.chetyrbok@gmail.com>
2015-01-05 01:08:03 +0300

    created list, add, edit, delete pages for employees

---
+++ api/employee/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+from employee import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.employee_list, name='employee_list'),
+    url(r'^add/$', views.employee_add, name='employee_add'),
+    url(r'^edit/(?P<employee_id>\d+)/$', views.employee_edit, name='employee_edit'),
+    url(r'^delete/$', views.employee_delete, name='employee_delete'),
+)
--- api/employee/views.py
+++ api/employee/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
-# Create your views here.
+from employee.models import Employee
+
+def employee_list(request):
+    employees = Employee.objects.all()
+
+    return render(request, 'employee/list.html', {'employees': employees})
+
+def employee_add(request):
+    return HttpResponse('Add employee')
+
+def employee_edit(request, employee_id):
+    try:
+        employee = Employee.objects.get(pk=employee_id)
+    except Employee.DoesNotExist:
+        raise Http404
+    return render(request, 'employee/edit.html', {'employee': employee})
+
+def employee_delete(request):
+    return HttpResponse('Edit delete')
