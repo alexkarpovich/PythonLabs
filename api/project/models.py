94f7a2ddd7f89da214ccb2039874adc82567c4f1
siarheipashkevich <siarheipashkevich@gmail.com>
2015-01-05 20:29:43 +0300

    Added models project and project_status, added to admin

---
+++ api/project/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    customer = models.CharField(max_length=64)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField()
+    status = models.ForeignKey('ProjectStatus')
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectStatus(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
