ae4e42d0c74417ea36d80b01c03fce0ee7d7828c
Yury Chetyrbok <yury.chetyrbok@gmail.com>
2015-01-07 23:46:27 +0300

    created employees list page

--- api/employee/urls.py
+++ api/employee/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     url(r'^$', views.employee_list, name='employee_list'),
     url(r'^add/$', views.employee_add, name='employee_add'),
     url(r'^edit/(?P<employee_id>\d+)/$', views.employee_edit, name='employee_edit'),
-    url(r'^delete/$', views.employee_delete, name='employee_delete'),
+    url(r'^delete/(?P<employee_id>\d+)$', views.employee_delete, name='employee_delete'),
 )
