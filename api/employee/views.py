66da40ab29bcb7230a90790abe17a66e21c55dbb
Alexander Amyago <alexande.amyago@itechart-group.com>
2015-01-08 14:24:24 +0300

    Added languages to employee module

--- api/employee/views.py
+++ api/employee/views.py
@@ -1,22 +1,56 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
+from django.core.context_processors import csrf
+from forms import EmployeeForm
 
-from models import Employee
+from models import Employee, Language
+
 
 def employee_list(request):
     employees = Employee.objects.all()
 
     return render(request, 'employee/list.html', {'employees': employees})
 
+
 def employee_add(request):
-    return HttpResponse('Add employee')
+    if request.method == "POST":
+        f = EmployeeForm(request.POST)
+        if f.is_valid():
+            f.save()
+            return HttpResponseRedirect('/employee')
+    else:
+        f = EmployeeForm()
+
+    args = {}
+    args.update(csrf(request))
+    args['form'] = f
+
+    return render(request, 'employee/add.html', {'form': EmployeeForm()})
+
 
 def employee_edit(request, employee_id):
     try:
         employee = Employee.objects.get(pk=employee_id)
     except Employee.DoesNotExist:
         raise Http404
+
     return render(request, 'employee/edit.html', {'employee': employee})
 
 def employee_delete(request):
     return HttpResponse('Edit delete')
+
+def languages_list(request):
+    languages = Language.objects.all()
+
+    return render(request, 'language/list.html', {'languages': languages})
+
+def language_edit(request, language_id):
+    try:
+        language = Language.objects.get(pk=language_id)
+    except Language.DoesNotExist:
+        raise Http404
+
+    return render(request, 'language/edit.html', {'language': language})
+
+def language_add(request):
+    return render(request, 'language/add.html', {})
